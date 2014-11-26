fa68479e96dbd1fae620116c4a2f1c4fc29729fd
Matt Lutrick <mattlutrick@Kyles-Mac-mini.local>
2014-11-26 17:06:09 -0600

    First commit

---
+++ src/CSVSplitter.java
@@ -0,0 +1,138 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVSplitter 
+{
+	BufferedReader reader = null;
+	public CSVSplitter()
+	{}
+	public void split(String toSplitCSV)
+	{
+		String[] patientList = this.getPatientList(toSplitCSV);
+		String[][] commaSplitList = this.commaSplitList(patientList);
+		//String[][] removedQuotes = this.removeQuotes(commaSplitList);
+		this.print(commaSplitList);
+	}
+	
+	public String[] getPatientList(String toSplitCSV)
+	{
+		int afterHeader = 3;
+		int counter = 0;
+		String workingLine = "";
+		String[] patientList = null;
+		try
+		{
+			this.openReader(toSplitCSV);
+			while ((workingLine = reader.readLine()) != null)
+			{
+				//System.out.print("Made inside getPatientList()'s first while loop");
+				if (counter >= afterHeader)
+				{
+					counter++;
+				}
+			}
+			this.closeReader();
+			this.openReader(toSplitCSV);
+			patientList = new String[counter-3];
+			while ((workingLine = reader.readLine()) != null)
+			{
+				if (counter >= afterHeader)
+				{
+					patientList[counter-3] = workingLine;
+				}
+				counter++;
+			}
+		}
+		catch (IOException e)
+		{
+			
+		}
+		finally
+		{
+			this.closeReader();
+		}
+		return patientList;
+	}
+	
+	public String[][] commaSplitList(String[] patientList)
+	{
+		int counterForPatients = 0;
+		int counterForData = 1;
+		String[][] returnList = new String[patientList.length/10][10];
+		String[] commaSplitLine = new String[10];
+		while (counterForPatients < patientList.length/10)
+		{
+			commaSplitLine = patientList[counterForPatients].split(",");
+			while(counterForData < 10)
+			{
+				returnList[counterForPatients][counterForData] = commaSplitLine[counterForData];
+				counterForData++;
+			}
+			counterForData = 0;
+			counterForPatients++;
+		}
+		return returnList;
+	}
+	
+	
+	public String removeQuotes(String toRemove)
+	{
+		char[] holder = toRemove.toCharArray();
+		String toReturn = "";
+		for(char c : holder)
+		{
+			if (!(c == '\"'))
+			{
+					toReturn += c;
+			}
+		}
+		return toReturn;
+	}
+	
+	public void openReader(String fileName)
+	{
+		try
+		{
+			reader = new BufferedReader(new FileReader(fileName));
+		}
+		catch (FileNotFoundException e)
+		{
+			
+		}
+	}
+	
+	public void closeReader()
+	{
+		if (reader != null)
+		{
+			try
+			{
+				reader.close();
+			}
+			catch (IOException e)
+			{
+				
+			}
+		}
+	}
+	
+	public void print(String[][] toPrint)
+	{
+		int counter1 = 0;
+		int counter2 = 0;
+		
+		while (counter1 < toPrint.length)
+		{
+			while (counter2 < toPrint[counter1].length)
+			{
+				System.out.print(toPrint[counter1][counter2]);
+				counter2++;
+			}
+			System.out.println();
+			counter2 = 0;
+			counter1++;
+		}
+	}
+}
---
+++ src/TestRun.java
@@ -0,0 +1,94 @@
+import java.io.*;
+
+
+
+
+public class TestRun 
+{
+
+	public static void main(String[] args) 
+	{
+		CSVSplitter thisSplitter = new CSVSplitter();
+		File testFile = new File("hi.txt");
+		String testString = "\"Johnston, James  - 27381 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES 1029330,,A7039 \n\"Yoda, Master  - 27361 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES 1035442,,A7038 \n\"Prait, Chris L - 93047 : 1\",5154 MAUDELAYNE DR S,MOBILE,AL,36693-0000,(251) 214 - 7503,001 - CIGNA GOVERNMENT SERVICES JURISDICTION C,MED WRITEOFF,,A7034 \n\"Johnston, James  - 23412 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES A7035,,A7035 \n\"Prait, Chris L - 12345 : 1\",5154 MAUDELAYNE DR S,MOBILE,AL,36693-0000,(251) 214 - 7503,001 - CIGNA GOVERNMENT SERVICES JURISDICTION C,MED WRITEOFF,,A7037";
+
+		/*try 
+		{
+			FileWriter writer = new FileWriter(testFile);
+			writer.write("\"Johnston, James  - 27381 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES 1029330,,A7039 \n\"Yoda, Master  - 27361 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES 1035442,,A7038 \n\"Prait, Chris L - 93047 : 1\",5154 MAUDELAYNE DR S,MOBILE,AL,36693-0000,(251) 214 - 7503,001 - CIGNA GOVERNMENT SERVICES JURISDICTION C,MED WRITEOFF,,A7034 \n\"Johnston, James  - 23412 : 1\",807 SHELTON BEACH RD #33,SARALAND,AL,36571-0000,(205) 368 - 5265,950 - VIVA HEALTH,RES A7035,,A7035 \n\"Prait, Chris L - 12345 : 1\",5154 MAUDELAYNE DR S,MOBILE,AL,36693-0000,(251) 214 - 7503,001 - CIGNA GOVERNMENT SERVICES JURISDICTION C,MED WRITEOFF,,A7037");
+			writer.flush();
+			writer.close();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}*/
+		String deQuoted = thisSplitter.removeQuotes(testString);
+		System.out.println(deQuoted);
+		String[] lineSplit = deQuoted.split("\n");
+		String[] holding = new String[Math.multiplyExact(lineSplit.length, 12)];
+		String[] holding2 = new String[Math.multiplyExact(lineSplit.length, 16)];
+		String[] finalHolding = new String[holding2.length];
+		int counter = 0;
+		for (String s : lineSplit)
+		{
+			String[] deCommaed = s.split(",");
+			for (String s2 : deCommaed)
+			{
+				holding[counter] = s2;
+				counter++;
+			}
+		}
+		counter = 0;
+		/*for(String s3 : holding)
+		{
+			if (s3 != null)
+			{
+				System.out.println(s3);
+			}
+		}*/
+		for (String s4 : holding)
+		{
+			String[] deHyphened = s4.split("-");
+			for (String s5 : deHyphened)
+			{
+				holding2[counter] = s5;
+				counter++;
+			}
+		}
+		counter = 0;
+		for (String s6 : holding2)
+		{
+			String[] deColoned = s6.split(":");
+			for (String s7 : deColoned)
+			{
+				finalHolding[counter] = s7;
+				counter++;
+			}
+		}
+		
+		for (String s8 : finalHolding)
+		{
+			System.out.println(s8);
+		}
+		/*char[] chars = new char[1000];
+		try 
+		{
+			FileReader reader = new FileReader(testFile);
+			reader.read(chars);
+			reader.close();
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		for(char c : chars)
+		{
+			System.out.print(c);
+		}*/
+	}	
+}
